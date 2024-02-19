e78db2439ea0feaef71ebd214c6b8a156723f264
dheepan <csdheepan18@gmail.com>
2024-02-19 17:47:23 +0530

    <dheepan> 19-02-2024 1.Code clean up and standardization done.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,37 +11,37 @@ import { InMemoryCache } from '../shared/service/memory-cache.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
+
 export class LoginComponent implements OnInit {
 
-
-
+  // Variable Decleration
   banner = "assets/images/login-banner.png";
   hide = true;
   userLoggedIn: boolean = true;
   showAlert: boolean = false;
   signupForm: FormGroup = Object.create(null);
-
   loginForm: FormGroup = Object.create(null);
-
   signUpPayload !: SignUp;
-
   loginPayload !: Login;
 
+  //Constructor : Utilize Dependency Injection here.
   constructor(private fb: FormBuilder, private authenticationServices: AuthenticationServices,
-    private router: Router, private store: InMemoryCache , private userService: UserService) {
+    private router: Router, private store: InMemoryCache, private userService: UserService) { }
 
 
-  }
-
+  //Onload function
   ngOnInit(): void {
+
+    // Signup Form Validation
     this.signupForm = this.fb.group({
-      name: [null, [Validators.required,Validators.maxLength(20),Validators.pattern("^[a-zA-Z ]+$")]],
+      name: [null, [Validators.required, Validators.maxLength(20), Validators.pattern("^[a-zA-Z ]+$")]],
       email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required,Validators.pattern(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[0-9]).{8,}$/)]],
+      password: [null, [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*[0-9]).{8,}$/)]],
       confirmPassword: [null, [Validators.required]]
     });
 
-
+    // Login Form Validation
     this.loginForm = this.fb.group({
       userName: [null, [Validators.required, Validators.email]],
       passcode: [null, [Validators.required]],
@@ -54,32 +54,66 @@ export class LoginComponent implements OnInit {
   }
 
 
-
+  //method to check Password and confirm password are same.
   checkPasswordMatch(): void {
+
+    //form control value for  password and confirmPassword.
     const password = this.signupForm.controls['password']?.value;
     const confirmPassword = this.signupForm.controls['confirmPassword']?.value;
 
     if (password != null && confirmPassword != null) {
+
+      // if password === Confirm password return null otherwise we set error passwordMismatch
       this.signupForm.controls['confirmPassword']?.setErrors(password === confirmPassword ? null : { 'passwordMismatch': true });
     }
+
   }
 
+  //method to handle view for login and signup field.
   handleView(value: any) {
 
+    //show login form field
     if (value == "login") {
       this.userLoggedIn = true;
     }
+
+    //show signup form field
     else if (value == "signup") {
       this.userLoggedIn = false;
     }
-    this.showAlert = false;
+
+    //reset the form
     this.signupForm.reset();
     this.loginForm.reset();
+    this.showAlert = false;
   }
 
+
+  //method for to restrict user to resitered using same email id.
+  checkValidation(value: string) {
+    let email = value ? value : "";
+
+    if (email != "" && this.signupForm.controls['email'].valid) {
+
+      //get all register details.
+      this.userService.getAllUser().subscribe((data: any) => {
+
+        // check email id already exist or not using some method (Js method).
+        let emailExists = data.some((user: any) => user.email === email);
+
+        if (emailExists) {
+          // Set an error for the email form control if email already exist.
+          this.signupForm.controls['email'].setErrors({ 'emailExists': true });
+        }
+      });
+    }
+  }
+
+  //method for login
   login() {
+
+    //retrieve all register user details.
     this.authenticationServices.getRegisterUser().subscribe((data: any[]) => {
-      console.log(data);
 
       // Extract login details from the form
       const loginDetails = {
@@ -92,70 +126,53 @@ export class LoginComponent implements OnInit {
 
         this.showAlert = false;
 
-        // Check if the email and password match
+        // Check if the email and password match ie user all already logged in.
         if (user.email === loginDetails.userName && user.password === loginDetails.passcode) {
           console.log('Login successful');
-          this.router.navigate(["profile/feed"]);
+
+          //store individual user details
           this.store.setItem("USER_DETAILS", JSON.stringify(user));
+
+          //reset form
           this.signupForm.reset();
           this.loginForm.reset();
+
+          //navigate to view profile
+          this.router.navigate(["profile/feed"]);
           return;
         }
-        else {
-          // If no match found,
-          console.log('Invalid login credentials');
 
+        // If no match found ie login details are anot valid / not registered.
+        else {
+          console.log('Invalid login credentials');
           this.showAlert = true;
         }
       }
-
-
     });
   }
 
-
-
+  //method for signup
   signup() {
 
-    let profileImg = "assets/images/person.jpg"  //by default we show this particular img
+    let profileImg = "assets/images/person.jpg"  //by default we store this particular img.
 
+    //payload for to store details in database.
     this.signUpPayload = {
       id: "",
       name: this.signupForm.controls['name'].value,
       email: this.signupForm.controls['email'].value,
       password: this.signupForm.controls['password'].value,
-      profileImg : profileImg
+      profileImg: profileImg
     }
 
 
+    //service call - to store the register details in database
     this.authenticationServices.userSignUp(this.signUpPayload);
 
-    this.userLoggedIn = true;
-
+    //reset form
     this.signupForm.reset();
     this.loginForm.reset();
-  }
+    this.userLoggedIn = true;
 
-
-  checkValidation(value:string){
-
-
-    let email = value ? value : "";
-
-
-    if(email != "" && this.signupForm.controls['email'].valid){
-      this.userService.getAllUser().subscribe((data: any) => {
-
-       // some method is used to test whether at least one element in the array passes. It returns true
-        let emailExists = data.some((user: any) => user.email === email);
-      
-        if (emailExists) {
-          // Set an error for the email form control
-          this.signupForm.controls['email'].setErrors({ 'emailExists': true });
-        }
-      });
-      
-    }
-  
   }
 }
